162f35697129bd1f621e9f2aa8617ab40c9d06a0
Alex Garel <alex@garel.org>
2022-10-11 09:32:40 +0200

    feat: add a thread safe parse function (#82)
    * feat: add a thread safe parse function
    
    fixes:
    - #72
    
    * test: fix threads test

---
+++ tests/test_thread.py
@@ -0,0 +1,33 @@
+import queue
+import threading
+
+from luqum.parser import parser
+from luqum.thread import parse
+
+
+def test_thread_parse():
+
+    result_queue = queue.Queue()
+    qs1 = """
+        (title:"foo bar" AND body:"quick fox") OR title:fox AND
+        (title:"foo bar" AND body:"quick fox") OR
+        title:fox AND (title:"foo bar" AND body:"quick fox") OR
+        title:fox AND (title:"foo bar" AND body:"quick fox") OR
+        title:fox AND (title:"foo bar" AND body:"quick fox") OR title:fox
+    """
+    expected_tree = parser.parse(qs1)
+
+    def run(q):
+        parse(qs1)
+        tree = parse(qs1)
+        q.put(tree)
+
+    # make concurrents calls
+    threads = [threading.Thread(target=run, args=(result_queue,)) for i in range(100)]
+    for thread in threads:
+        thread.start()
+    for thread in threads:
+        thread.join()
+    assert result_queue.qsize() == 100
+    for i in range(100):
+        assert result_queue.get() == expected_tree
