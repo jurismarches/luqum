a609940ea3cfe5e5e3dca4f11f527347a19abf52
Eric Masseran <rico.masseran@gmail.com>
2025-01-08 15:07:50 +0100

    Document unused params in thread.parse()

--- luqum/thread.py
+++ luqum/thread.py
@@ -8,8 +8,11 @@ thread_local = threading.local()
 def parse(input=None, lexer=None, debug=False, tracking=False):
     """A (hopefully) thread safe version of :py:meth:`luqum.parser.parse`
 
-    PLY is not thread safe because of its lexer state, but cloning it we can be thread safe.
-    see: https://github.com/jurismarches/luqum/issues/72
+    PLY is not thread safe because of its lexer state, but cloning it we can be
+    thread safe. see: https://github.com/jurismarches/luqum/issues/72
+
+    Warning: The parameter ``lexer``, ``debug`` and ``tracking`` are not used.
+    They are still present for signature compatibility.
     """
     if not hasattr(thread_local, "lexer"):
         thread_local.lexer = parser.lexer.clone()
