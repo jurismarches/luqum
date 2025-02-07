6d1f479bc09d622d05d2e2edb96bb5c8c287cb2d
Alex Garel <alex@garel.org>
2025-02-07 10:49:42 +0100

    chore: drop python3.9 support

--- setup.py
+++ setup.py
@@ -31,7 +31,6 @@ setup(
         'License :: OSI Approved :: Apache Software License',
         'Intended Audience :: Developers',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
         'Programming Language :: Python :: 3.12',
