991e26f4b1565db3f6ece3aedbc033d1690025f9
Eric Masseran <rico.masseran@gmail.com>
2025-02-17 09:28:26 +0100

    Lexer global state management (#109)
    Co-authored-by: Alex Garel <alex@garel.org>

--- luqum/thread.py
+++ luqum/thread.py
@@ -1,9 +1,6 @@
 import threading
 
-from ply import lex
-
 from . import parser
-
 
 thread_local = threading.local()
 
@@ -11,9 +8,12 @@ thread_local = threading.local()
 def parse(input=None, lexer=None, debug=False, tracking=False):
     """A (hopefully) thread safe version of :py:meth:`luqum.parser.parse`
 
-    PLY is not thread safe because of its lexer state, but cloning it we can be thread safe.
-    see: https://github.com/jurismarches/luqum/issues/72
+    PLY is not thread safe because of its lexer state, but cloning it we can be
+    thread safe. see: https://github.com/jurismarches/luqum/issues/72
+
+    Warning: The parameter ``lexer``, ``debug`` and ``tracking`` are not used.
+    They are still present for signature compatibility.
     """
     if not hasattr(thread_local, "lexer"):
-        thread_local.lexer = lex.lexer.clone()
+        thread_local.lexer = parser.lexer.clone()
     return parser.parser.parse(input, lexer=thread_local.lexer)
